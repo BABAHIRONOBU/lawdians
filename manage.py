bce7055b92bf7f8717b3711e1f89f629ea1665ee
Aaron <zaiyou12@gmail.com>
2017-06-01 14:05:34 +0900

    Large file structure

--- manage.py
+++ manage.py
@@ -1,9 +1,16 @@
 import os
 
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Shell
+
+from app import db
+from app.models import User
 
 app = Flask(__name__)
+
+manager = Manager(app)
+migrate = Migrate(app, db)
 
 
 # controllers
@@ -57,6 +64,19 @@ def reset_password():
     return render_template("_reset-password.html")
 
 
+def make_shell_context():
+    return dict(app=app, db=db, User=User)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    manager.run()
